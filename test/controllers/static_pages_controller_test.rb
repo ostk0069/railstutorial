48bfe6b4cbccf0f64c5e7082dc80022c3e5a7594
Takuma Osada <judgment9ttk069@gmail.com>
2017-05-24 22:41:35 +0900

    railsep4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,7 +24,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get about" do
-  get static_pages_about_url
-  assert_response :success
+    get static_pages_about_url
+    assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
 end
